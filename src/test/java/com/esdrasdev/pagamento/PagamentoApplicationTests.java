8ce3a63856905b7f18db1eb7f04bbaacab467aff
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-10-21 08:15:51 -0300

    First commit

---
+++ src/test/java/com/esdrasdev/pagamento/PagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esdrasdev.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
