42bb0f913cec0f4995b275288fdb7bcc7bc6cfb9
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-10-22 16:39:07 -0300

    Create pagamentoService

---
+++ src/main/java/com/esdrasdev/pagamento/business/PagamentoService.java
@@ -0,0 +1,17 @@
+package com.esdrasdev.pagamento.business;
+
+import com.esdrasdev.pagamento.controller.request.CartaoRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PagamentoService {
+
+    private final PagamentoClient pagamentoClient;
+
+    public Boolean verificaPagamento(CartaoRequestDTO cartaoRequestDTO) {
+        return pagamentoClient.verificaCartao(cartaoRequestDTO);
+    }
+
+}
